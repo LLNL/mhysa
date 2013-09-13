f20b20106e47e8c76332efdcd56d5d13b9ebb82d
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2013-09-12 19:57:40 -0500

    Added the function to apply the boundary conditions

---
+++ src/HyParFunctions/ApplyBoundaryConditions.c
@@ -0,0 +1,25 @@
+#include <basic.h>
+#include <arrayfunctions.h>
+#include <mpivars.h>
+#include <boundaryconditions.h>
+#include <hypar.h>
+
+int ApplyBoundaryConditions(void *s,void *m,double *x) 
+{
+  HyPar           *solver   = (HyPar*)          s;
+  DomainBoundary  *boundary = (DomainBoundary*) solver->boundary;
+  MPIVariables    *mpi      = (MPIVariables*)   m;
+  int             nb        = solver->nBoundaryZones;
+  int             ierr      = 0;
+
+
+  /* Apply domain boundary conditions to p */
+  int n;
+  for (n = 0; n < nb; n++) {
+    ierr = boundary[n].BCFunction(&boundary[n],mpi,solver->ndims,solver->nvars,
+                                  solver->dim_local,solver->ghosts,x);
+    CHECKERR(ierr);
+  }
+
+  return(0);
+}
