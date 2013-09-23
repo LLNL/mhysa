697603b42fb67265783e63bb42b40b4f92028370
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2013-09-23 15:17:39 -0500

    Bugfix: transfer to/from PETSc were the other way round...

--- src/PETScFunctions/TransferToPETSc.c
+++ src/PETScFunctions/TransferToPETSc.c
@@ -15,8 +15,8 @@ int TransferToPETSc(double *u,Vec Y,void *ctxt)
 
   int *index = (int*) calloc (solver->ndims,sizeof(int));
   ierr = VecGetArray(Y,&Yarr); CHKERRQ(ierr);
-  ierr = ArrayCopynD(solver->ndims,Yarr,u,solver->dim_local,0,
-                     solver->ghosts,index,solver->nvars); CHECKERR(ierr);
+  ierr = ArrayCopynD(solver->ndims,u,Yarr,solver->dim_local,solver->ghosts,
+                     0,index,solver->nvars); CHECKERR(ierr);
   ierr = VecRestoreArray(Y,&Yarr); CHKERRQ(ierr);
 
   free(index);
