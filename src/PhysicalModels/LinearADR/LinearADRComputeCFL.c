c53f3032c11cb57a854e854d0bade21bc6e13739
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2013-10-13 00:53:01 -0400

    Updated LinearADR functions to use the GetCoordinate() function to get x and dx

--- src/PhysicalModels/LinearADR/LinearADRComputeCFL.c
+++ src/PhysicalModels/LinearADR/LinearADRComputeCFL.c
@@ -12,18 +12,16 @@ double LinearADRComputeCFL(void *s,void *m,double dt,double t)
   int     nvars  = solver->nvars;
   int     ghosts = solver->ghosts;
   int     *dim   = solver->dim_local;
-  double  *dxinv = solver->dxinv;
 
-  int     offset  = 0;
   double  max_cfl = 0;
   for (d = 0; d < ndims; d++) {
     for (i = 0; i < dim[d]; i++) {
       for (v = 0; v < nvars; v++) {
-        double local_cfl = params->a[nvars*d+v]*dt*dxinv[offset+ghosts+i];
+        double dxinv = solver->GetCoordinate(d,i,dim,ghosts,solver->dxinv);
+        double local_cfl = params->a[nvars*d+v]*dt*dxinv;
         if (local_cfl > max_cfl) max_cfl = local_cfl;
       }
     }
-    offset += (dim[d]+2*ghosts);
   }
 
   return(max_cfl);
--- src/PhysicalModels/LinearADR/LinearADRComputeDiffNumber.c
+++ src/PhysicalModels/LinearADR/LinearADRComputeDiffNumber.c
@@ -11,19 +11,17 @@ double LinearADRComputeDiffNumber(void *s,void *m,double dt,double t)
   int     ndims  = solver->ndims;
   int     nvars  = solver->nvars;
   int     *dim   = solver->dim_local;
-  double  *dxinv = solver->dxinv;
 
-  int     offset  = 0;
   double  max_diffno = 0;
   for (d = 0; d < ndims; d++) {
     for (i = 0; i < dim[d]; i++) {
       for (v = 0; v < nvars; v++) {
+        double dxinv = solver->GetCoordinate(d,i,dim,ghosts,solver->dxinv);
         double local_diffno =   params->d[nvars*d+v] * dt 
                               * dxinv[offset+i] * dxinv[offset+i];
         if (local_diffno > max_diffno) max_diffno = local_diffno;
       }
     }
-    offset += dim[d];
   }
 
   return(max_diffno);
