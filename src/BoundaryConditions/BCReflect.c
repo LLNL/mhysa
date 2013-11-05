3b27f46a3a3c41e4d0a163b4093a01f9007bb1a6
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2013-11-04 19:46:16 -0600

    Added reflection boundary condition

---
+++ src/BoundaryConditions/BCReflect.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <basic.h>
+#include <arrayfunctions.h>
+#include <boundaryconditions.h>
+
+int BCReflect(void *b,void *m,int ndims,int nvars,int *size,int ghosts,double *phi)
+{
+  DomainBoundary *boundary = (DomainBoundary*) b;
+  int            ierr      = 0;
+
+  int dim   = boundary->dim;
+  int face  = boundary->face;
+  int var   = boundary->var;
+
+  if (boundary->on_this_proc) {
+    int *bounds = (int*) calloc (ndims,sizeof(int));
+    int *indexb = (int*) calloc (ndims,sizeof(int));  /* boundary index */
+    int *indexi = (int*) calloc (ndims,sizeof(int));  /* interior index */
+    ierr = ArraySubtract1D_int(bounds,boundary->ie,boundary->is,ndims); CHECKERR(ierr);
+    ierr = ArraySetValue_int  (indexb,ndims,0);                         CHECKERR(ierr);
+    int done = 0;
+    while (!done) {
+      int p1, p2;
+      ierr = ArrayCopy1D_int(indexb,indexi,ndims);              CHECKERR(ierr);
+      ierr = ArrayAdd1D_int (indexi,indexi,boundary->is,ndims); CHECKERR(ierr);
+      if      (face ==  1) indexi[dim] = ghosts-1-indexb[dim];
+      else if (face == -1) indexi[dim] = size[dim]-indexb[dim];
+      else return(1);
+      p1 = ArrayIndex1D(ndims,size  ,indexb,boundary->is,ghosts);
+      p2 = ArrayIndex1D(ndims,size  ,indexi,NULL        ,ghosts);
+      phi[nvars*p1+var] = -phi[nvars*p2+var];
+      done = ArrayIncrementIndex(ndims,bounds,indexb);
+    }
+    free(bounds);
+    free(indexb);
+    free(indexi);
+  }
+  return(0);
+}
+
