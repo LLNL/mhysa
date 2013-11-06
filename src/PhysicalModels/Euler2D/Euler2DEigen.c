9641019a8b8ca61e7cfbc525ac38009fc5105b50
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2013-11-05 19:49:29 -0600

    Changed the eigenvectors for 2D Euler to those by Rohde

--- src/PhysicalModels/Euler2D/Euler2DEigen.c
+++ src/PhysicalModels/Euler2D/Euler2DEigen.c
@@ -6,26 +6,33 @@
 #include <physicalmodels/euler2d.h>
 #include <hypar.h>
 
+/*
+
+  Rohde, A., "Eigenvalues and Eigenvectors of the Euler Equations
+  in General Geometries", AIAA 2001-2609
+
+*/
+
 inline int Euler2DGetFlowVar (double*,double*,double*,double*,double*,double*,void*);
 
 inline int Euler2DEigenvalues(double *u,double **D,void *p,int dir)
 {
-  Euler2D  *param  = (Euler2D*)  p;
-  int      ierr    = 0;
-  double   gamma   = param->gamma;
-  double   rho,vx,vy,e,P,c,vn;
+  Euler2D *param  = (Euler2D*)  p;
+  int     ierr    = 0;
+  double  gamma   = param->gamma;
+  double  rho,vx,vy,e,P,c,vn;
 
   ierr = Euler2DGetFlowVar(u,&rho,&vx,&vy,&e,&P,param); CHECKERR(ierr);
   c    = sqrt(gamma*P/rho);
 
   if      (dir == _XDIR_) vn = vx;
   else if (dir == _YDIR_) vn = vy;
-  else                    vn = 0;
+  else                    vn = 0.0;
 
-  D[0][0] = vn-c; D[0][1] = 0;      D[0][2] = 0;    D[0][3] = 0;
-  D[1][0] = 0;    D[1][1] = vn;     D[1][2] = 0;    D[1][3] = 0;
-  D[2][0] = 0;    D[2][1] = 0;      D[2][2] = vn+c; D[2][3] = 0;
-  D[3][0] = 0;    D[3][1] = 0;      D[3][2] = 0;    D[3][3] = vn;
+  D[0][0] = vn-c;   D[0][1] = 0;    D[0][2] = 0;      D[0][3] = 0;
+  D[1][0] = 0;      D[1][1] = vn;   D[1][2] = 0;      D[1][3] = 0;
+  D[2][0] = 0;      D[2][1] = 0;    D[2][2] = vn+c;   D[2][3] = 0;
+  D[3][0] = 0;      D[3][1] = 0;    D[3][2] = 0;      D[3][3] = vn;
 
   return(0);
 }
@@ -33,45 +40,67 @@ inline int Euler2DEigenvalues(double *u,double **D,void *p,int dir)
 inline int Euler2DLeftEigenvectors(double *u,double **L,void *p,int dir)
 {
   Euler2D *param  = (Euler2D*)  p;
-  int     ierr    = 0;
-  double  gamma   = param->gamma, K=gamma-1.0;
-  double  rho,vx,vy,e,P,c,csq;
-  double  nx,ny,lx,ly;
-  double  qsq,qn,ql;
+  int     ierr  = 0;
+  double  ga    = param->gamma, ga_minus_one=ga-1.0;
+  double  rho,vx,vy,e,P,a,un,ek;
+  double  nx = 0,ny = 0;
 
   ierr = Euler2DGetFlowVar(u,&rho,&vx,&vy,&e,&P,param); CHECKERR(ierr);
-  c    = sqrt(gamma*P/rho); csq = c*c;
-  
-  if (dir == _XDIR_) {
-    nx = 1.0; ny = 0.0;
-    lx = 0.0; ly = 1.0;
-  } else if (dir == _YDIR_) {
-    nx = 0.0; ny = 1.0;
-    lx = 1.0; ly = 0.0;
-  } else {
-    nx = 0.0; ny = 0.0;
-    lx = 0.0; ly = 0.0;
-  }
-  qn  = vx*nx + vy*ny;
-  ql  = vx*lx + vy*ly;
-  qsq = vx*vx + vy*vy;
+	ek = 0.5 * (vx*vx + vy*vy);
+	a = sqrt(ga * P / rho);
 
-  L[0][0] = (K*qsq)/(4*csq) + qn/(2*c);
-  L[0][1] = - (K/(2*csq)*vx + nx/(2*c));
-  L[0][2] = - (K/(2*csq)*vy + ny/(2*c));
-  L[0][3] = K/(2*csq);
-  L[1][0] = 1.0 - (K*qsq)/(2*csq);
-  L[1][1] = K*vx/csq;
-  L[1][2] = K*vy/csq;
-  L[1][3] = - K/csq;
-  L[2][0] = (K*qsq)/(4*csq) - qn/(2*c);
-  L[2][1] = - (K/(2*csq)*vx - nx/(2*c));
-  L[2][2] = - (K/(2*csq)*vy - ny/(2*c));
-  L[2][3] = K/(2*csq);
-  L[3][0] = -ql;
-  L[3][1] = lx;
-  L[3][2] = ly;
-  L[3][3] = 0;
+  if (dir == _XDIR_) { 
+    un = vx; 
+    nx = 1.0; 
+
+		L[0][0] = (ga_minus_one*ek + a*un) / (2*a*a);
+		L[0][1] = ((-ga_minus_one)*vx - a*nx) / (2*a*a);
+		L[0][2] = ((-ga_minus_one)*vy - a*ny) / (2*a*a);
+		L[0][3] = ga_minus_one / (2*a*a);
+
+		L[1][0] = (a*a - ga_minus_one*ek) / (a*a);
+		L[1][1] = (ga_minus_one*vx) / (a*a);
+		L[1][2] = (ga_minus_one*vy) / (a*a);
+		L[1][3] = (-ga_minus_one) / (a*a);
+
+		L[2][0] = (ga_minus_one*ek - a*un) / (2*a*a);
+		L[2][1] = ((-ga_minus_one)*vx + a*nx) / (2*a*a);
+		L[2][2] = ((-ga_minus_one)*vy + a*ny) / (2*a*a);
+		L[2][3] = ga_minus_one / (2*a*a);
+
+		L[3][0] = (vy - un*ny) / nx;
+		L[3][1] = ny;
+		L[3][2] = (ny*ny - 1.0) / nx;
+		L[3][3] = 0.0;
+
+  } else if (dir == _YDIR_) { 
+    un = vy; 
+    ny = 1.0; 
+
+		L[0][0] = (ga_minus_one*ek+a*un) / (2*a*a);
+		L[0][1] = ((1.0-ga)*vx - a*nx) / (2*a*a);
+		L[0][2] = ((1.0-ga)*vy - a*ny) / (2*a*a);
+		L[0][3] = ga_minus_one / (2*a*a);
+ 
+		L[1][0] = (a*a-ga_minus_one*ek) / (a*a);
+		L[1][1] = ga_minus_one*vx / (a*a);
+		L[1][2] = ga_minus_one*vy / (a*a);
+		L[1][3] = (1.0 - ga) / (a*a);
+ 
+		L[2][0] = (ga_minus_one*ek-a*un) / (2*a*a);
+		L[2][1] = ((1.0-ga)*vx + a*nx) / (2*a*a);
+		L[2][2] = ((1.0-ga)*vy + a*ny) / (2*a*a);
+		L[2][3] = ga_minus_one / (2*a*a);
+
+		L[3][0] = (un*nx-vx) / ny;
+		L[3][1] = (1.0 - nx*nx) / ny;
+		L[3][2] = - nx;
+		L[3][3] = 0;
+
+  } else {
+    fprintf(stderr,"Error in Euler2DLeftEigenvectors(): invalid dir!\n");
+    return(1);
+  }
 
   return(0);
 }
@@ -79,46 +108,68 @@ inline int Euler2DLeftEigenvectors(double *u,double **L,void *p,int dir)
 inline int Euler2DRightEigenvectors(double *u,double **R,void *p,int dir)
 {
   Euler2D *param  = (Euler2D*)  p;
-  int     ierr    = 0;
-  double  gamma   = param->gamma;
-  double  rho,vx,vy,e,P,c;
-  double  nx,ny,lx,ly;
-  double  qsq,qn,ql,H;
+  int     ierr = 0;
+  double  ga   = param->gamma, ga_minus_one = ga-1.0;
+  double  rho,vx,vy,e,P,un,ek,a,h0;
+  double  nx = 0,ny = 0;
 
   ierr = Euler2DGetFlowVar(u,&rho,&vx,&vy,&e,&P,param); CHECKERR(ierr);
-  c    = sqrt(gamma*P/rho);
-  H    = (e+P)/rho;
+	ek   = 0.5 * (vx*vx + vy*vy);
+	a    = sqrt(ga * P / rho);
+  h0   = a*a / ga_minus_one + ek;
 
-  if (dir == _XDIR_) {
-    nx = 1.0; ny = 0.0;
-    lx = 0.0; ly = 1.0;
-  } else if (dir == _YDIR_) {
-    nx = 0.0; ny = 1.0;
-    lx = 1.0; ly = 0.0;
+	if (dir == _XDIR_) {
+  	un = vx;
+    nx = 1.0;
+
+		R[0][0] = 1.0;
+		R[1][0] = vx - a*nx;
+		R[2][0] = vy - a*ny;
+		R[3][0] = h0 - a*un;
+
+		R[0][1] = 1.0;
+		R[1][1] = vx;
+		R[2][1] = vy;
+		R[3][1] = ek;
+
+		R[0][2] = 1.0;
+		R[1][2] = vx + a*nx;
+		R[2][2] = vy + a*ny;
+		R[3][2] = h0 + a*un;
+
+		R[0][3] = 0.0;
+		R[1][3] = ny;
+		R[2][3] = -nx;
+		R[3][3] = vx*ny - vy*nx;
+
+	} else if (dir == _YDIR_) {
+    un = vy;
+    ny = 1.0;
+
+		R[0][0] = 1.0;
+		R[1][0] = vx - a*nx;
+		R[2][0] = vy - a*ny;
+		R[3][0] = h0 - a*un;
+
+		R[0][1] = 1.0;
+		R[1][1] = vx;
+		R[2][1] = vy;
+		R[3][1] = ek;
+
+		R[0][2] = 1.0;
+		R[1][2] = vx + a*nx;
+		R[2][2] = vy + a*ny;
+		R[3][2] = h0 + a*un;
+
+		R[0][3] = 0;
+		R[1][3] = ny;
+		R[2][3] = -nx;
+		R[3][3] = vx*ny-vy*nx;
+
   } else {
-    nx = 0.0; ny = 0.0;
-    lx = 0.0; ly = 0.0;
+    fprintf(stderr,"Error in Euler2DRightEigenvectors(): invalid dir!\n");
+    return(1);
   }
-  qn  = vx*nx + vy*ny;
-  ql  = vx*lx + vy*ly;
-  qsq = vx*vx + vy*vy;
-
-  R[0][0] = 1.0;
-  R[0][1] = 1.0;
-  R[0][2] = 1.0;
-  R[0][3] = 0.0;
-  R[1][0] = vx - c*nx;;
-  R[1][1] = vx;
-  R[1][2] = vx + c*nx;
-  R[1][3] = lx;
-  R[2][0] = vy - c*ny;
-  R[2][1] = vy;
-  R[2][2] = vy + c*ny;
-  R[2][3] = ly;
-  R[3][0] = H - qn*c;
-  R[3][1] = qsq / 2.0;
-  R[3][2] = H + qn*c;
-  R[3][3] = ql;
 
   return(0);
 }
