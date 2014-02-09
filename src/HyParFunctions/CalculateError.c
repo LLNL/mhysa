ea0a1742e9d653a2414addc5ef6d2a3d6f854b00
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-02-08 22:35:07 -0600

    Exact solution file can also be read as binary or ascii

--- src/HyParFunctions/CalculateError.c
+++ src/HyParFunctions/CalculateError.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <basic.h>
 #include <arrayfunctions.h>
@@ -10,7 +11,7 @@ int CalculateError(void *s,void *m)
 {
   HyPar         *solver     = (HyPar*)        s;
   MPIVariables  *mpi        = (MPIVariables*) m;
-  int           exact_flag  = 0, d, i, ferr;
+  int           exact_flag  = 0, d, i, ferr, total_size;
   double        sum         = 0, global_sum = 0;
   _DECLARE_IERR_;
 
@@ -19,48 +20,90 @@ int CalculateError(void *s,void *m)
   if (!mpi->rank) {
     int size,offset;
 
-    /* Reading grid and exact solution */
-    FILE *in; in = fopen("exact.inp","r");
-    if (!in) {
-      solver->error[0] = solver->error[1] = solver->error[2] = 0;
-      exact_flag = 0;
-      ug = xg = NULL;
-    } else {
-      exact_flag = 1;
-      /* allocate global solution array */
-      size = solver->npoints_global*solver->nvars;
-      ug = (double*) calloc(size,sizeof(double));
-      size = 0;
-      for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
-      xg      = (double*) calloc(size,sizeof(double));
+    if (!strcmp(solver->ip_file_type,"ascii")) {
 
-      printf("Reading grid and exact solution from file \"exact.inp\".\n");
-      /* read grid, though not necessary (so that file format is consistent) */
-      offset = 0;
-      for (d = 0; d < solver->ndims; d++) {
-        for (i = 0; i < solver->dim_global[d]; i++) ferr = fscanf(in,"%lf",&xg[i+offset]);
-        offset += solver->dim_global[d];
-      }
-      /* read solution */
-      for (i = 0; i < solver->nvars; i++) {
-        int *index = solver->index;
-        int done = 0; _ArraySetValue_(index,solver->ndims,0);
-        while (!done) {
-          int p; _ArrayIndex1D_(solver->ndims,solver->dim_global,index,0,p);
-          ferr = fscanf(in,"%lf",&ug[p*solver->nvars+i]);
-          if (ferr != 1) return(1);
-          _ArrayIncrementIndex_(solver->ndims,solver->dim_global,index,done);
+      FILE *in; in = fopen("exact.inp","r");
+      if (!in) {
+        solver->error[0] = solver->error[1] = solver->error[2] = 0;
+        exact_flag = 0;
+        ug = xg = NULL;
+      } else {
+        exact_flag = 1;
+        /* allocate global solution array */
+        size = solver->npoints_global*solver->nvars;
+        ug = (double*) calloc(size,sizeof(double));
+        size = 0;
+        for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
+        xg      = (double*) calloc(size,sizeof(double));
+
+        printf("Reading grid and exact solution from ASCII file \"exact.inp\".\n");
+        /* read grid, though not necessary (so that file format is consistent) */
+        offset = 0;
+        for (d = 0; d < solver->ndims; d++) {
+          for (i = 0; i < solver->dim_global[d]; i++) ferr = fscanf(in,"%lf",&xg[i+offset]);
+          offset += solver->dim_global[d];
+        }
+        /* read solution */
+        for (i = 0; i < solver->nvars; i++) {
+          int *index = solver->index;
+          int done = 0; _ArraySetValue_(index,solver->ndims,0);
+          while (!done) {
+            int p; _ArrayIndex1D_(solver->ndims,solver->dim_global,index,0,p);
+            ferr = fscanf(in,"%lf",&ug[p*solver->nvars+i]);
+            if (ferr != 1) return(1);
+            _ArrayIncrementIndex_(solver->ndims,solver->dim_global,index,done);
+          }
+        }
+        fclose(in);
+
+        /* write the exact solution to file in the same format as the computed solution */
+        /* to enable comparisons and plotting together                                  */
+        if (solver->WriteOutput) {
+          char filename[_MAX_STRING_SIZE_] = "op_exact.dat";
+          IERR solver->WriteOutput(solver->ndims,solver->nvars,solver->dim_global,xg,ug,
+                                   filename,solver->index); CHECKERR(ierr);
         }
       }
-      fclose(in);
 
-      /* write the exact solution to file in the same format as the computed solution */
-      /* to enable comparisons and plotting together                                  */
-      if (solver->WriteOutput) {
-        char filename[_MAX_STRING_SIZE_] = "op_exact.dat";
-        IERR solver->WriteOutput(solver->ndims,solver->nvars,solver->dim_global,xg,ug,
+    } else if ((!strcmp(solver->ip_file_type,"bin")) || (!strcmp(solver->ip_file_type,"binary"))) {
+
+      FILE *in; in = fopen("exact.inp","rb");
+      if (!in) {
+        solver->error[0] = solver->error[1] = solver->error[2] = 0;
+        exact_flag = 0;
+        ug = xg = NULL;
+      } else {
+        exact_flag = 1;
+        /* allocate global solution array */
+        size = solver->npoints_global*solver->nvars;
+        ug = (double*) calloc(size,sizeof(double));
+        size = 0;
+        for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
+        xg      = (double*) calloc(size,sizeof(double));
+
+        printf("Reading grid and exact solution from binary file \"exact.inp\".\n");
+
+        /* read grid, though not necessary (so that file format is consistent) */
+        total_size = 0;
+        for (d = 0; d < solver->ndims; d++) total_size += solver->dim_global[d];
+        fread(xg, sizeof(double), total_size, in);
+
+        /* read solution */
+        total_size = 1;
+        for (d = 0; d < solver->ndims; d++) total_size *= solver->dim_global[d]; total_size *= solver->nvars;
+        fread(ug, sizeof(double), total_size, in);
+
+        fclose(in);
+
+        /* write the exact solution to file in the same format as the computed solution */
+        /* to enable comparisons and plotting together                                  */
+        if (solver->WriteOutput) {
+          char filename[_MAX_STRING_SIZE_] = "op_exact.dat";
+          IERR solver->WriteOutput(solver->ndims,solver->nvars,solver->dim_global,xg,ug,
                                    filename,solver->index); CHECKERR(ierr);
+        }
       }
+
     }
 
   } else ug = xg = NULL;
