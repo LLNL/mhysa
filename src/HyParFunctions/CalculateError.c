b709f2bd51ad96e393335815666ee7f738bd697d
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-02-19 14:05:24 -0600

    Making reading of exact solution consistent with reading initial solution

--- src/HyParFunctions/CalculateError.c
+++ src/HyParFunctions/CalculateError.c
@@ -7,134 +7,28 @@
 #include <mpivars.h>
 #include <hypar.h>
 
+int ExactSolution(void*,void*,double*,int*);
+
 int CalculateError(void *s,void *m)
 {
   HyPar         *solver     = (HyPar*)        s;
   MPIVariables  *mpi        = (MPIVariables*) m;
-  int           exact_flag  = 0, d, i, ferr, total_size;
+  int           exact_flag  = 0, i, size;
   double        sum         = 0, global_sum = 0;
+  double        *uex        = NULL;
   _DECLARE_IERR_;
 
-  /* Allocate arrays for reading in the exact solution */
-  double *ug = NULL, *xg = NULL; 
-  if (!mpi->rank) {
-    int size,offset;
+  size = 1;
+  for (i = 0; i < solver->ndims; i++) 
+    size *= (solver->dim_local[i]+2*solver->ghosts);
 
-    if (!strcmp(solver->ip_file_type,"ascii")) {
 
-      FILE *in; in = fopen("exact.inp","r");
-      if (!in) {
-        solver->error[0] = solver->error[1] = solver->error[2] = 0;
-        exact_flag = 0;
-        ug = xg = NULL;
-      } else {
-        exact_flag = 1;
-        /* allocate global solution array */
-        size = solver->npoints_global*solver->nvars;
-        ug = (double*) calloc(size,sizeof(double));
-        size = 0;
-        for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
-        xg      = (double*) calloc(size,sizeof(double));
-
-        printf("Reading grid and exact solution from ASCII file \"exact.inp\".\n");
-        /* read grid, though not necessary (so that file format is consistent) */
-        offset = 0;
-        for (d = 0; d < solver->ndims; d++) {
-          for (i = 0; i < solver->dim_global[d]; i++) ferr = fscanf(in,"%lf",&xg[i+offset]);
-          offset += solver->dim_global[d];
-        }
-        /* read solution */
-        for (i = 0; i < solver->nvars; i++) {
-          int *index = solver->index;
-          int done = 0; _ArraySetValue_(index,solver->ndims,0);
-          while (!done) {
-            int p; _ArrayIndex1D_(solver->ndims,solver->dim_global,index,0,p);
-            ferr = fscanf(in,"%lf",&ug[p*solver->nvars+i]);
-            if (ferr != 1) return(1);
-            _ArrayIncrementIndex_(solver->ndims,solver->dim_global,index,done);
-          }
-        }
-        fclose(in);
-
-        /* write the exact solution to file in the same format as the computed solution */
-        /* to enable comparisons and plotting together                                  */
-        if (solver->WriteOutput) {
-          char filename[_MAX_STRING_SIZE_] = "op_exact.dat";
-          IERR solver->WriteOutput(solver->ndims,solver->nvars,solver->dim_global,xg,ug,
-                                   filename,solver->index); CHECKERR(ierr);
-        }
-      }
-
-    } else if ((!strcmp(solver->ip_file_type,"bin")) || (!strcmp(solver->ip_file_type,"binary"))) {
-
-      FILE *in; in = fopen("exact.inp","rb");
-      size_t bytes;
-      if (!in) {
-        solver->error[0] = solver->error[1] = solver->error[2] = 0;
-        exact_flag = 0;
-        ug = xg = NULL;
-      } else {
-        exact_flag = 1;
-        /* allocate global solution array */
-        size = solver->npoints_global*solver->nvars;
-        ug = (double*) calloc(size,sizeof(double));
-        size = 0;
-        for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
-        xg      = (double*) calloc(size,sizeof(double));
-
-        printf("Reading grid and exact solution from binary file \"exact.inp\".\n");
-
-        /* read grid, though not necessary (so that file format is consistent) */
-        total_size = 0;
-        for (d = 0; d < solver->ndims; d++) total_size += solver->dim_global[d];
-        bytes = fread(xg, sizeof(double), total_size, in); 
-        if ((int)bytes != total_size) {
-          fprintf(stderr, "Error in CalculateError(): unable to read in grid. Expected %d, Read %d.\n",
-                  total_size, (int)bytes);
-        }
-
-        /* read solution */
-        total_size = 1;
-        for (d = 0; d < solver->ndims; d++) total_size *= solver->dim_global[d]; total_size *= solver->nvars;
-        bytes = fread(ug, sizeof(double), total_size, in);
-        if ((int)bytes != total_size) {
-          fprintf(stderr, "Error in CalculateError(): unable to read in solution. Expected %d, Read %d.\n",
-                  total_size, (int)bytes);
-        }
-
-        fclose(in);
-
-        /* write the exact solution to file in the same format as the computed solution */
-        /* to enable comparisons and plotting together                                  */
-        if (solver->WriteOutput) {
-          char filename[_MAX_STRING_SIZE_] = "op_exact.bin";
-          IERR solver->WriteOutput(solver->ndims,solver->nvars,solver->dim_global,xg,ug,
-                                   filename,solver->index); CHECKERR(ierr);
-        }
-      }
-
-    }
-
-  } else ug = xg = NULL;
-
-  /* Broadcast exact_flag to all processes */
-  IERR MPIBroadcast_integer(&exact_flag,1,0,&mpi->world);
-
-  if (!exact_flag)  return(0);  /* No exact solution */
-
-  /* allocate local exact solution array */
-  int size = 1;
-  for (d=0; d<solver->ndims; d++) size *= (solver->dim_local[d]+2*solver->ghosts);
-  double *uex = (double*) calloc (size*solver->nvars,sizeof(double));
-  
-  /* partition global exact solution array to all processes */
-  IERR MPIPartitionArraynD(solver->ndims,mpi,(mpi->rank?NULL:ug),uex,
-                             solver->dim_global,solver->dim_local,
-                             solver->ghosts,solver->nvars); CHECKERR(ierr);
-
-  /* free up global exact solution arrays */
-  if (ug)  free(ug);
-  if (xg)  free(xg);
+  IERR ExactSolution(solver,mpi,uex,&exact_flag); CHECKERR(ierr);
+  if (!exact_flag) {
+    /* No exact solution */
+    solver->error[0] = solver->error[1] = solver->error[2] = 0.0;
+    return(0);  
+  }
 
   /* compute error = difference between exact and numerical solution */
   _ArrayAXPY_(solver->u,-1.0,uex,size*solver->nvars);
---
+++ src/HyParFunctions/ExactSolution.c
@@ -0,0 +1,243 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <basic.h>
+#include <arrayfunctions.h>
+#include <mpivars.h>
+#include <hypar.h>
+
+static int ExactSolutionSerial    (void*, void*, double*, int*);
+static int ExactSolutionParallel  (void*, void*, double*, int*);
+
+int ExactSolution(void *s, void *m, double *uex, int *flag)
+{
+  HyPar  *solver = (HyPar*) s;
+  if      (!strcmp(solver->input_mode,"serial"))    return(ExactSolutionSerial    (s,m,uex,flag));
+  else if (!strcmp(solver->input_mode,"parallel"))  return(ExactSolutionParallel  (s,m,uex,flag));
+  else {
+    fprintf(stderr,"Error: Illegal value (%s) for input_mode (may be \"serial\" or \"parallel\"\n",
+            solver->input_mode);
+    return(1);
+  }
+}
+
+int ExactSolutionSerial(void *s, void *m, double *uex, int *exact_flag)
+{
+  HyPar         *solver = (HyPar*)        s;
+  MPIVariables  *mpi    = (MPIVariables*) m;
+  int           i, d, ferr;
+  double        *ug = NULL, *xg = NULL; 
+  _DECLARE_IERR_;
+
+  *exact_flag = 0; uex = NULL;
+  /* Only root process reads in exact solution file */
+  if (!mpi->rank) {
+
+    if (!strcmp(solver->ip_file_type,"ascii")) {
+
+      FILE *in; in = fopen("exact.inp","r");
+      if (!in) *exact_flag = 0;
+      else {
+        *exact_flag = 1;
+        /* Reading exact solution */
+        printf("Reading exact solution from ASCII file \"exact.inp\" (Serial mode).\n");
+        int size,offset;
+        /* allocate global solution array */
+        size  = solver->npoints_global*solver->nvars;
+        ug    = (double*) calloc(size,sizeof(double));
+        size  = 0; for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
+        xg    = (double*) calloc(size,sizeof(double));
+
+        /* read grid (not necessary but to keep initial and exact solutions file formats same */
+        offset = 0;
+        for (d = 0; d < solver->ndims; d++) {
+          for (i = 0; i < solver->dim_global[d]; i++) ferr = fscanf(in,"%lf",&xg[i+offset]);
+          offset += solver->dim_global[d];
+        }
+
+        /* read solution */
+        for (i = 0; i < solver->nvars; i++) {
+          int *index = solver->index;
+          int done = 0; _ArraySetValue_(index,solver->ndims,0);
+          while (!done) {
+            int p; _ArrayIndex1D_(solver->ndims,solver->dim_global,index,0,p);
+            ferr = fscanf(in,"%lf",&ug[p*solver->nvars+i]);
+            if (ferr != 1) return(1);
+            _ArrayIncrementIndex_(solver->ndims,solver->dim_global,index,done);
+          }
+        }
+
+        fclose(in);
+      }
+
+    } else if ((!strcmp(solver->ip_file_type,"bin")) || (!strcmp(solver->ip_file_type,"binary"))) {
+
+      FILE *in; in = fopen("exact.inp","rb");
+      if (!in) *exact_flag = 0;
+      else {
+        *exact_flag = 1;
+        printf("Reading exact solution from binary file \"exact.inp\" (Serial mode).\n");
+        size_t bytes;
+        int size,offset;
+        /* allocate global solution array */
+        size = solver->npoints_global*solver->nvars;
+        ug = (double*) calloc(size,sizeof(double));
+        size = 0; for (d=0; d<solver->ndims; d++) size += solver->dim_global[d];
+        xg      = (double*) calloc(size,sizeof(double));
+
+        /* read grid */
+        size = 0;
+        for (d = 0; d < solver->ndims; d++) size += solver->dim_global[d];
+        bytes = fread(xg, sizeof(double), size, in);
+        if ((int)bytes != size) {
+          fprintf(stderr,"Error in ExactSolution(): Unable to read grid. Expected %d, Read %d.\n",
+                  size, (int)bytes);
+        }
+
+        /* read solution */
+        size = 1;
+        for (d = 0; d < solver->ndims; d++) size *= solver->dim_global[d]; size *= solver->nvars;
+        bytes = fread(ug, sizeof(double), size, in);
+        if ((int)bytes != size) {
+          fprintf(stderr,"Error in ExactSolution(): Unable to read solution. Expected %d, Read %d.\n",
+                  size, (int)bytes);
+        }
+
+        fclose(in);
+      }
+
+    }
+
+  }
+
+  /* Broadcast exact_flag to all processes */
+  IERR MPIBroadcast_integer(exact_flag,1,0,&mpi->world); CHECKERR(ierr);
+
+  if (*exact_flag) {
+    /* allocate local exact solution array */
+    int size = 1;
+    for (d=0; d<solver->ndims; d++) size *= (solver->dim_local[d]+2*solver->ghosts);
+    uex = (double*) calloc (size*solver->nvars,sizeof(double));
+    /* partition global exact solution array to all processes */
+    IERR MPIPartitionArraynD(solver->ndims,mpi,(mpi->rank?NULL:ug),uex,
+                             solver->dim_global,solver->dim_local,
+                             solver->ghosts,solver->nvars); CHECKERR(ierr);
+    /* free global arrays */
+    if (!mpi->rank) {
+      free(ug);
+      free(xg);
+    }
+  }
+
+  return(0);
+}
+
+int ExactSolutionParallel(void *s, void *m, double *uex, int *exact_flag)
+{
+  HyPar         *solver = (HyPar*)        s;
+  MPIVariables  *mpi    = (MPIVariables*) m;
+  _DECLARE_IERR_;
+
+  *exact_flag = 0, uex = NULL;
+  if (!strcmp(solver->ip_file_type,"ascii")) {
+
+    fprintf(stderr,"Error in ExactSolutionParallel(): Reading ASCII exact solution file in parallel not yet supported. Choose the binary option.\n");
+    return(1);
+
+  } else if ((!strcmp(solver->ip_file_type,"binary")) || (!strcmp(solver->ip_file_type,"bin"))) {
+
+    FILE *in; in = fopen("exact.inp","rb");
+    if (!in) *exact_flag = 0;
+    else {
+      *exact_flag = 1;
+      int myrank = mpi->rank, ndims  = solver->ndims, ghosts = solver->ghosts;
+      int rank[ndims+1], size[ndims], flag = 0, nvars, ferr, total, n;
+
+      double *x, *u;
+      total = 0; for (n = 0; n < ndims; n++) total += solver->dim_local[n];
+      x = (double*) calloc (total, sizeof(double));
+      total = solver->nvars; for (n = 0; n < ndims; n++) total *= solver->dim_local[n];
+      u = (double*) calloc (total, sizeof(double));
+
+      printf("Reading exact solution from binary file \"exact.inp\" (Parallel mode).\n");
+      int done = 0;
+      while((!feof(in)) && (!done)) {
+        ferr = fread(rank,sizeof(int),ndims+1,in); 
+        if (ferr != (ndims+1)) {
+          fprintf(stderr,"Error in reading binary file exact.inp in parallel mode on rank %d.\n", myrank);
+          return(1);
+        }
+        ferr = fread(size,sizeof(int),ndims,in);
+        if (ferr != (ndims+1)) {
+          fprintf(stderr,"Error in reading binary file exact.inp in parallel mode on rank %d.\n", myrank);
+          return(1);
+        }
+        ferr = fread(&nvars,sizeof(int),1,in);
+        if (ferr != 1) {
+          fprintf(stderr,"Error in reading binary file exact.inp in parallel mode on rank %d.\n", myrank);
+          return(1);
+        }
+        if (rank[ndims] == myrank) {
+          /* checks */
+          int check = 1;
+          for (n = 0; n < ndims; n++) {
+            if (rank[n] != mpi->ip[n])            check = 0;
+            if (size[n] != solver->dim_local[n])  check = 0;
+          }
+          if (nvars != solver->nvars) check = 0;
+          if (!check) {
+            fprintf(stderr,"Error in ExactSolutionParallel(): Inconsistent data read on rank %d.\n",myrank);
+            return(1);
+          }
+          /* read grid */
+          total = 0; for (n = 0; n < ndims; n++) total += size[n];
+          ferr = fread(x,sizeof(double),total,in);
+          if (ferr != total) {
+            fprintf(stderr,"Error in reading binary file exact.inp in parallel mode on rank %d.\n", myrank);
+            return(1);
+          }
+          /* read exact solution */
+          total = nvars; for (n = 0; n < ndims; n++) total *= size[n];
+          ferr = fread(u,sizeof(double),total,in);
+          if (ferr != total) {
+            fprintf(stderr,"Error in reading binary file exact.inp in parallel mode on rank %d.\n", myrank);
+            return(1);
+          }
+          flag = 1;
+          done = 1;
+        } else {
+          int n, offset1 = 0, offset2 = nvars;
+          for (n = 0; n < ndims; n++) {
+            offset1 += size[n];
+            offset2 *= size[n];
+          }
+          ferr = fseek(in,sizeof(double)*(offset1+offset2),SEEK_CUR);
+          if (ferr) {
+            fprintf(stderr,"Error in reading binary file exact.inp in parallel mode on rank %d.\n", myrank);
+            return(1);
+          }
+        }
+      }
+      fclose(in);
+
+      total = solver->nvars; for (n = 0; n < ndims; n++) total *= (solver->dim_local[n]+2*ghosts);
+      uex = (double*) calloc (total, sizeof(double));
+
+      int index[ndims];
+      IERR ArrayCopynD(ndims,u,uex,solver->dim_local,0,ghosts,index,solver->nvars); CHECKERR(ierr);
+
+      free(x);
+      free(u);
+    }
+
+    return(0);
+
+  } else {
+
+    fprintf(stderr,"Error in ExactSolutionParallel(): Illegal value (%s) for ip_file type. May be \"ascii\", \"binary\" or \"bin\".\n",
+            solver->ip_file_type);
+    return(1);
+
+  }
+
+}
