931281dde6e730c002049129163b4b07851171ff
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-02-25 14:14:31 -0600

    For parallel file read, rank 0 reads each sub-domain and sends it to the corresponding process, rather than all processes reading the files at once!

--- src/HyParFunctions/ExactSolution.c
+++ src/HyParFunctions/ExactSolution.c
@@ -135,6 +135,7 @@ int ExactSolutionParallel(void *s, void *m, double *uex, int *exact_flag)
   _DECLARE_IERR_;
 
   *exact_flag = 0;
+
   if (!strcmp(solver->ip_file_type,"ascii")) {
 
     fprintf(stderr,"Error in ExactSolutionParallel(): Reading ASCII exact solution file in parallel not yet supported. Choose the binary option.\n");
@@ -142,87 +143,138 @@ int ExactSolutionParallel(void *s, void *m, double *uex, int *exact_flag)
 
   } else if ((!strcmp(solver->ip_file_type,"binary")) || (!strcmp(solver->ip_file_type,"bin"))) {
 
-    FILE *in; in = fopen("exact_par.inp","rb");
-    if (!in) *exact_flag = 0;
-    else {
-      *exact_flag = 1;
-      int myrank = mpi->rank, ndims  = solver->ndims, ghosts = solver->ghosts;
-      int rank[ndims+1], size[ndims], flag = 0, nvars, ferr, total, n;
+    int     ndims = solver->ndims, d, n, *recv_int;
+    double  *recv_double;
 
-      double *x, *u;
-      total = 0; for (n = 0; n < ndims; n++) total += solver->dim_local[n];
-      x = (double*) calloc (total, sizeof(double));
-      total = solver->nvars; for (n = 0; n < ndims; n++) total *= solver->dim_local[n];
-      u = (double*) calloc (total, sizeof(double));
+    /* allocate arrays to receive data in */
+    int sizex = 0;             for (d=0; d<ndims; d++) sizex += solver->dim_local[d];
+    int sizeu = solver->nvars; for (d=0; d<ndims; d++) sizeu *= solver->dim_local[d];
+    int total_size = sizex + sizeu;
+    recv_int    = (int*)    calloc (2*(ndims+1),sizeof(int  ));
+    recv_double = (double*) calloc (total_size,sizeof(double));
 
-      if (!myrank) printf("Reading exact solution from binary file \"exact_par.inp\" (Parallel mode).\n");
-      int done = 0;
-      while((!feof(in)) && (!done)) {
-        ferr = fread(rank,sizeof(int),ndims+1,in); 
-        if (ferr != (ndims+1)) {
-          fprintf(stderr,"Error (1) in reading binary file exact_par.inp in parallel mode on rank %d.\n", myrank);
-          return(1);
-        }
-        ferr = fread(size,sizeof(int),ndims,in);
-        if (ferr != ndims) {
-          fprintf(stderr,"Error (2) in reading binary file exact_par.inp in parallel mode on rank %d.\n", myrank);
-          return(1);
-        }
-        ferr = fread(&nvars,sizeof(int),1,in);
-        if (ferr != 1) {
-          fprintf(stderr,"Error (3) in reading binary file exact_par.inp in parallel mode on rank %d.\n", myrank);
-          return(1);
-        }
-        if (rank[ndims] == myrank) {
-          /* checks */
-          int check = 1;
-          for (n = 0; n < ndims; n++) {
-            if (rank[n] != mpi->ip[n])            check = 0;
-            if (size[n] != solver->dim_local[n])  check = 0;
-          }
-          if (nvars != solver->nvars) check = 0;
-          if (!check) {
-            fprintf(stderr,"Error in ExactSolutionParallel(): Inconsistent data read on rank %d.\n",myrank);
-            return(1);
-          }
-          /* read grid */
-          total = 0; for (n = 0; n < ndims; n++) total += size[n];
-          ferr = fread(x,sizeof(double),total,in);
-          if (ferr != total) {
-            fprintf(stderr,"Error (4) in reading binary file exact_par.inp in parallel mode on rank %d.\n", myrank);
-            return(1);
-          }
-          /* read exact solution */
-          total = nvars; for (n = 0; n < ndims; n++) total *= size[n];
-          ferr = fread(u,sizeof(double),total,in);
-          if (ferr != total) {
-            fprintf(stderr,"Error (5) in reading binary file exact_par.inp in parallel mode on rank %d.\n", myrank);
-            return(1);
-          }
-          flag = 1;
-          done = 1;
-        } else {
-          int n, offset1 = 0, offset2 = nvars;
-          for (n = 0; n < ndims; n++) {
-            offset1 += size[n];
-            offset2 *= size[n];
-          }
-          ferr = fseek(in,sizeof(double)*(offset1+offset2),SEEK_CUR);
-          if (ferr) {
-            fprintf(stderr,"Error (6) in reading binary file exact_par.inp in parallel mode on rank %d.\n", myrank);
-            return(1);
-          }
-        }
-      }
+    /* check if exact solution file exists */
+    if (!mpi->rank) {
+      FILE *in;
+      in = fopen("exact_par.inp","rb");
+      if (in) *exact_flag = 1;
+      else    *exact_flag = 0;
       fclose(in);
+    }
+    /* Broadcast exact_flag to all processes */
+    IERR MPIBroadcast_integer(exact_flag,1,0,&mpi->world); CHECKERR(ierr);
+
+    if (*exact_flag) {
+
+      if (!mpi->rank) {
+
+        /* rank 0 opens the file, reads in the data and sends it to corresponding process */
+        FILE  *in;
+        int   ferr;
+        int *flag = (int*) calloc (mpi->nproc, sizeof(int));
+        for (n=0; n<mpi->nproc; n++) flag[n] = 0;
+        in = fopen("exact_par.inp","rb");
+
+        int count = 0;
+        while ((!feof(in)) && (count < mpi->nproc)) {
+          int rank[ndims+1],size[ndims],nvars;
+          ferr = fread(rank,sizeof(int),ndims+1,in);
+          if (ferr != (ndims+1)) {
+            fprintf(stderr,"Error (1) in reading binary file exact_par.inp in parallel mode on rank %d.\n", mpi->rank);
+            return(1);
+          }
+          ferr = fread(size,sizeof(int),ndims,in);
+          if (ferr != ndims) {
+            fprintf(stderr,"Error (2) in reading binary file exact_par.inp in parallel mode on rank %d.\n", mpi->rank);
+            return(1);
+          }
+          ferr = fread(&nvars,sizeof(int),1,in);
+          if (ferr != 1) {
+            fprintf(stderr,"Error (3) in reading binary file exact_par.inp in parallel mode on rank %d.\n", mpi->rank);
+            return(1);
+          }
+          int sizex = 0;    for (d=0; d<ndims; d++) sizex += size[d];
+          int sizeu = nvars;for (d=0; d<ndims; d++) sizeu *= size[d];
+          int total_size = sizex + sizeu;
+          if (!rank[ndims]) {
+            /* self */
+            ferr = fread(recv_double,sizeof(double),total_size,in);
+            if (ferr != total_size) {
+              fprintf(stderr,"Error (4) in reading binary file exact_par.inp in parallel mode on rank %d.\n", mpi->rank);
+              return(1);
+            }
+            for (d=0; d<ndims+1; d++) recv_int[d]         = rank[d];
+            for (d=0; d<ndims  ; d++) recv_int[d+ndims+1] = size[d];
+            recv_int[2*ndims+1] = nvars;
+          } else {
+#ifndef serial
+            /* allocate data array to send */
+            int    *send_int     = (int*)     calloc (2*(ndims+1),sizeof(int   ));
+            double *send_double  = (double*)  calloc (total_size ,sizeof(double));
+            ferr = fread(send_double,sizeof(double),total_size,in);
+            if (ferr != total_size) {
+              fprintf(stderr,"Error (4) in reading binary file exact_par.inp in parallel mode on rank %d.\n", mpi->rank);
+              return(1);
+            }
+            for (d=0; d<ndims+1; d++) send_int[d]         = rank[d];
+            for (d=0; d<ndims  ; d++) send_int[d+ndims+1] = size[d];
+            send_int[2*ndims+1] = nvars;
+            /* send to the corresponding rank */
+            MPI_Request req[2] = {MPI_REQUEST_NULL,MPI_REQUEST_NULL};
+            MPI_Isend(send_int   ,2*(ndims+1),MPI_INT   ,rank[ndims],1247,mpi->world,&req[0]);
+            MPI_Isend(send_double,total_size ,MPI_DOUBLE,rank[ndims],1248,mpi->world,&req[1]);
+            MPI_Waitall(2,&req[0],MPI_STATUS_IGNORE);
+            free(send_int);
+            free(send_double);
+#else
+            fprintf(stderr,"Error in ExactSolutionParallel(): Code should not have reached here!\n");
+#endif
+          }
+          flag[rank[ndims]] = 1;
+          count++;
+        }
+
+        /* check if data for all the processes have been read */
+        int check = 1;
+        for (n = 0; n < mpi->nproc; n++) {
+          if (!flag[n]) {
+            fprintf(stderr,"Error in ExactSolutionParallel(): Data for rank %d not found.\n",n);
+            check = 0;
+          }
+        }
+        if (!check) return(1);
+
+        fclose(in);
+
+      } else {
+
+        /* receive the stuff from rank 0 */
+        MPI_Request req[2] = {MPI_REQUEST_NULL,MPI_REQUEST_NULL};
+        MPI_Irecv(recv_int   ,2*(ndims+1),MPI_INT   ,0,1247,mpi->world,&req[0]);
+        MPI_Irecv(recv_double,total_size ,MPI_DOUBLE,0,1248,mpi->world,&req[1]);
+        MPI_Waitall(2,&req[0],MPI_STATUS_IGNORE);
+
+      }
+
+      /* checks */
+      int check = 1;
+      for (n = 0; n < ndims; n++) {
+        if (recv_int[n]         != mpi->ip[n]          )  check = 0;
+        if (recv_int[n+ndims+1] != solver->dim_local[n])  check = 0;
+      }
+      if (recv_int[2*ndims+1] != solver->nvars) check = 0;
+      if (!check) {
+        fprintf(stderr,"Error in ExactSolutionParallel(): Inconsistent data read on rank %d.\n",mpi->rank);
+        return(1);
+      }
 
       int index[ndims];
-      IERR ArrayCopynD(ndims,u,uex,solver->dim_local,0,ghosts,index,solver->nvars); CHECKERR(ierr);
+      IERR ArrayCopynD(ndims,recv_double+sizex,uex,solver->dim_local,0,solver->ghosts,index,solver->nvars); CHECKERR(ierr);
 
-      free(x);
-      free(u);
+      free(recv_int);
+      free(recv_double);
+
     }
-
     return(0);
 
   } else {
@@ -232,5 +284,4 @@ int ExactSolutionParallel(void *s, void *m, double *uex, int *exact_flag)
     return(1);
 
   }
-
 }
--- src/HyParFunctions/InitialSolution.c
+++ src/HyParFunctions/InitialSolution.c
@@ -204,101 +204,135 @@ int InitialSolutionParallel(void *s, void *m)
 
   } else if ((!strcmp(solver->ip_file_type,"binary")) || (!strcmp(solver->ip_file_type,"bin"))) {
 
-    int myrank = mpi->rank;
-    int ndims  = solver->ndims;
-    int rank[ndims+1], size[ndims], nvars;
-    int ferr, total, n;
-    FILE *in;
+    int     ndims = solver->ndims, d, n, *recv_int;
+    double  *recv_double;
+    /* allocate arrays to receive data in */
+    int sizex = 0;             for (d=0; d<ndims; d++) sizex += solver->dim_local[d];
+    int sizeu = solver->nvars; for (d=0; d<ndims; d++) sizeu *= solver->dim_local[d];
+    int total_size = sizex + sizeu;
+    recv_int    = (int*)    calloc (2*(ndims+1),sizeof(int  ));
+    recv_double = (double*) calloc (total_size,sizeof(double));
 
-    double *x, *u;
-    total = 0; for (n = 0; n < ndims; n++) total += solver->dim_local[n];
-    x = (double*) calloc (total, sizeof(double));
-    total = solver->nvars; for (n = 0; n < ndims; n++) total *= solver->dim_local[n];
-    u = (double*) calloc (total, sizeof(double));
 
-    in = fopen("initial_par.inp","rb");
-    if (!in) {
-      fprintf(stderr,"Error in InitialSolutionParallel(): Could not open file initial_par.inp for reading.\n");
+    if (!mpi->rank) {
+
+      /* rank 0 opens the file, reads in the data and sends it to corresponding process */
+      FILE  *in;
+      int   ferr;
+      int *flag = (int*) calloc (mpi->nproc, sizeof(int));
+      for (n=0; n<mpi->nproc; n++) flag[n] = 0;
+      in = fopen("initial_par.inp","rb");
+      if (!in) {
+        fprintf(stderr,"Error in InitialSolutionParallel(): Could not open file initial_par.inp for reading.\n");
+        return(1);
+      }
+      int count = 0;
+      while ((!feof(in)) && (count < mpi->nproc)) {
+        int rank[ndims+1],size[ndims],nvars;
+        ferr = fread(rank,sizeof(int),ndims+1,in);
+        if (ferr != (ndims+1)) {
+          fprintf(stderr,"Error (1) in reading binary file initial_par.inp in parallel mode on rank %d, count %d.\n", mpi->rank,count);
+          return(1);
+        }
+        ferr = fread(size,sizeof(int),ndims,in);
+        if (ferr != ndims) {
+          fprintf(stderr,"Error (2) in reading binary file initial_par.inp in parallel mode on rank %d, count %d.\n", mpi->rank,count);
+          return(1);
+        }
+        ferr = fread(&nvars,sizeof(int),1,in);
+        if (ferr != 1) {
+          fprintf(stderr,"Error (3) in reading binary file initial_par.inp in parallel mode on rank %d, count %d.\n", mpi->rank,count);
+          return(1);
+        }
+        int sizex = 0;    for (d=0; d<ndims; d++) sizex += size[d];
+        int sizeu = nvars;for (d=0; d<ndims; d++) sizeu *= size[d];
+        int total_size = sizex + sizeu;
+        if (!rank[ndims]) {
+          /* self */
+          ferr = fread(recv_double,sizeof(double),total_size,in);
+          if (ferr != total_size) {
+            fprintf(stderr,"Error (4) in reading binary file initial_par.inp in parallel mode on rank %d, count %d.\n", mpi->rank,count);
+            return(1);
+          }
+          for (d=0; d<ndims+1; d++) recv_int[d]         = rank[d];
+          for (d=0; d<ndims  ; d++) recv_int[d+ndims+1] = size[d];
+          recv_int[2*ndims+1] = nvars;
+        } else {
+#ifndef serial
+          /* allocate data array to send */
+          int    *send_int     = (int*)     calloc (2*(ndims+1),sizeof(int   ));
+          double *send_double  = (double*)  calloc (total_size ,sizeof(double));
+          ferr = fread(send_double,sizeof(double),total_size,in);
+          if (ferr != total_size) {
+            fprintf(stderr,"Error (4) in reading binary file initial_par.inp in parallel mode on rank %d.\n", mpi->rank);
+            return(1);
+          }
+          for (d=0; d<ndims+1; d++) send_int[d]         = rank[d];
+          for (d=0; d<ndims  ; d++) send_int[d+ndims+1] = size[d];
+          send_int[2*ndims+1] = nvars;
+          /* send to the corresponding rank */
+          MPI_Request req[2] = {MPI_REQUEST_NULL,MPI_REQUEST_NULL};
+          MPI_Isend(send_int   ,2*(ndims+1),MPI_INT   ,rank[ndims],1247,mpi->world,&req[0]);
+          MPI_Isend(send_double,total_size ,MPI_DOUBLE,rank[ndims],1248,mpi->world,&req[1]);
+          MPI_Waitall(2,&req[0],MPI_STATUS_IGNORE);
+          free(send_int);
+          free(send_double);
+#else
+          fprintf(stderr,"Error in InitialSolutionParallel(): Code should not have reached here!\n");
+#endif
+        }
+        flag[rank[ndims]] = 1;
+        count++;
+      }
+      fclose(in);
+
+      /* check if data for all the processes have been read */
+      int check = 1;
+      for (n = 0; n < mpi->nproc; n++) {
+        if (!flag[n]) {
+          fprintf(stderr,"Error in InitialSolutionParallel(): Data for rank %d not found.\n",n);
+          check = 0;
+        }
+      }
+      if (!check) return(1);
+
+    } else {
+
+      /* receive the stuff from rank 0 */
+      MPI_Request req[2] = {MPI_REQUEST_NULL,MPI_REQUEST_NULL};
+      MPI_Irecv(recv_int   ,2*(ndims+1),MPI_INT   ,0,1247,mpi->world,&req[0]);
+      MPI_Irecv(recv_double,total_size ,MPI_DOUBLE,0,1248,mpi->world,&req[1]);
+      MPI_Waitall(2,&req[0],MPI_STATUS_IGNORE);
+
+    }
+
+    /* checks */
+    int check = 1;
+    for (n = 0; n < ndims; n++) {
+      if (recv_int[n]         != mpi->ip[n]          )  check = 0;
+      if (recv_int[n+ndims+1] != solver->dim_local[n])  check = 0;
+    }
+    if (recv_int[ndims]     != mpi->rank    ) check = 0;
+    if (recv_int[2*ndims+1] != solver->nvars) check = 0;
+    if (!check) {
+      fprintf(stderr,"Error in InitialSolutionParallel(): Inconsistent data read on rank %d.\n",mpi->rank);
       return(1);
     }
-    if (!myrank) printf("Reading grid and initial conditions from binary file \"initial_par.inp\" (Parallel mode).\n");
-    int done = 0;
-    while((!feof(in) && (!done))) {
-      ferr = fread(rank,sizeof(int),ndims+1,in); 
-      if (ferr != (ndims+1)) {
-        fprintf(stderr,"Error (1) in reading binary file initial_par.inp in parallel mode on rank %d.\n", myrank);
-        return(1);
-      }
-      ferr = fread(size,sizeof(int),ndims,in);
-      if (ferr != ndims) {
-        fprintf(stderr,"Error (2) in reading binary file initial_par.inp in parallel mode on rank %d.\n", myrank);
-        return(1);
-      }
-      ferr = fread(&nvars,sizeof(int),1,in);
-      if (ferr != 1) {
-        fprintf(stderr,"Error (3) in reading binary file initial_par.inp in parallel mode on rank %d.\n", myrank);
-        return(1);
-      }
-      if (rank[ndims] == myrank) {
-        /* checks */
-        int check = 1;
-        for (n = 0; n < ndims; n++) {
-          if (rank[n] != mpi->ip[n])            check = 0;
-          if (size[n] != solver->dim_local[n])  check = 0;
-        }
-        if (nvars != solver->nvars) check = 0;
-        if (!check) {
-          fprintf(stderr,"Error in InitialSolutionParallel(): Inconsistent data read on rank %d.\n",myrank);
-          return(1);
-        }
-        /* read grid */
-        total = 0; for (n = 0; n < ndims; n++) total += size[n];
-        ferr = fread(x,sizeof(double),total,in);
-        if (ferr != total) {
-          fprintf(stderr,"Error (4) in reading binary file initial_par.inp in parallel mode on rank %d.\n", myrank);
-          return(1);
-        }
-        /* read initial solution */
-        total = nvars; for (n = 0; n < ndims; n++) total *= size[n];
-        ferr = fread(u,sizeof(double),total,in);
-        if (ferr != total) {
-          fprintf(stderr,"Error (5) in reading binary file initial_par.inp in parallel mode on rank %d.\n", myrank);
-          return(1);
-        }
-        done = 1;
-      } else {
-        int n, offset1 = 0, offset2 = nvars;
-        for (n = 0; n < ndims; n++) {
-          offset1 += size[n];
-          offset2 *= size[n];
-        }
-        ferr = fseek(in,sizeof(double)*(offset1+offset2),SEEK_CUR);
-        if (ferr) {
-          fprintf(stderr,"Error (6) in reading binary file initial_par.inp in parallel mode on rank %d.\n", myrank);
-          return(1);
-        }
-      }
-    }
-    fclose(in);
 
-    if (!done) {
-      fprintf(stderr,"Error in InitialSolutionParallel(): data for rank %d not found!\n",myrank);
-      return(1);
-    }
-
+    /* copy the grid */
     int offset1 = 0, offset2 = 0;
     for (n = 0; n < ndims; n++) {
       int i;
-      for (i = 0; i < solver->dim_local[n]; i++) solver->x[i+offset1+ghosts] = x[i+offset2];
+      for (i = 0; i < solver->dim_local[n]; i++) solver->x[i+offset1+ghosts] = recv_double[i+offset2];
       offset1 += (solver->dim_local[n]+2*ghosts);
       offset2 += solver->dim_local[n];
     }
 
     int index[ndims];
-    IERR ArrayCopynD(ndims,u,solver->u,solver->dim_local,0,ghosts,index,solver->nvars); CHECKERR(ierr);
+    IERR ArrayCopynD(ndims,recv_double+sizex,solver->u,solver->dim_local,0,ghosts,index,solver->nvars); CHECKERR(ierr);
 
-    free(x);
-    free(u);
+    free(recv_int);
+    free(recv_double);
 
   } else {
 
