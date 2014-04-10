68dbb1f3a9ee7c6dfc713bcf964c9f656c9288ca
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-04-10 11:54:11 -0500

    NavierStokes3D: Bugfix in reading physics parameters

--- src/PhysicalModels/NavierStokes3D/NavierStokes3DInitialize.c
+++ src/PhysicalModels/NavierStokes3D/NavierStokes3DInitialize.c
@@ -59,11 +59,11 @@ int NavierStokes3DInitialize(void *s,void *m)
         } else if (!strcmp(word,"upwinding")) {
           ferr = fscanf(in,"%s",physics->upw_choice); if (ferr != 1) return(1);
         } else if (!strcmp(word,"Pr")) {
-          ferr = fscanf(in,"%s",physics->Pr); if (ferr != 1) return(1);
+          ferr = fscanf(in,"%lf",&physics->Pr); if (ferr != 1) return(1);
         } else if (!strcmp(word,"Re")) {
-          ferr = fscanf(in,"%s",physics->Re); if (ferr != 1) return(1);
+          ferr = fscanf(in,"%lf",&physics->Re); if (ferr != 1) return(1);
         } else if (!strcmp(word,"Minf")) {
-          ferr = fscanf(in,"%s",physics->Minf); if (ferr != 1) return(1);
+          ferr = fscanf(in,"%lf",&physics->Minf); if (ferr != 1) return(1);
         } else if (strcmp(word,"end")) {
           char useless[_MAX_STRING_SIZE_];
           ferr = fscanf(in,"%s",useless); if (ferr != 1) return(ferr);
