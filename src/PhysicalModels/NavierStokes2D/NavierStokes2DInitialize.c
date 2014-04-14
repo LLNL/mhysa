713e660a1ea225e4d3d21fbfe0ff8e24e1c64378
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-04-14 06:21:04 -0500

    NavierStokes2D: added the viscous term calculation function

--- src/PhysicalModels/NavierStokes2D/NavierStokes2DInitialize.c
+++ src/PhysicalModels/NavierStokes2D/NavierStokes2DInitialize.c
@@ -8,6 +8,7 @@
 #include <mpivars.h>
 #include <hypar.h>
 
+int    NavierStokes2DParabolicFunction (double*,double*,void*,void*,double);
 double NavierStokes2DComputeCFL        (void*,void*,double,double);
 int    NavierStokes2DFlux              (double*,double*,int,void*,double);
 int    NavierStokes2DUpwindRoe         (double*,double*,double*,double*,double*,double*,int,void*,double);
@@ -36,6 +37,11 @@ int NavierStokes2DInitialize(void *s,void *m)
 
   /* default values */
   physics->gamma = 1.4; 
+  physics->Pr     = 0.72;
+  physics->Re     = -1;
+  physics->Minf   = 1.0;
+  physics->C1     = 1.458e-6;
+  physics->C2     = 110.4;
   strcpy(physics->upw_choice,"roe");
 
   /* reading physical model specific inputs - all processes */
@@ -54,6 +60,12 @@ int NavierStokes2DInitialize(void *s,void *m)
           ferr = fscanf(in,"%lf",&physics->gamma); if (ferr != 1) return(1);
         } else if (!strcmp(word,"upwinding")) {
           ferr = fscanf(in,"%s",physics->upw_choice); if (ferr != 1) return(1);
+        } else if (!strcmp(word,"Pr")) {
+          ferr = fscanf(in,"%lf",&physics->Pr); if (ferr != 1) return(1);
+        } else if (!strcmp(word,"Re")) {
+          ferr = fscanf(in,"%lf",&physics->Re); if (ferr != 1) return(1);
+        } else if (!strcmp(word,"Minf")) {
+          ferr = fscanf(in,"%lf",&physics->Minf); if (ferr != 1) return(1);
         } else if (strcmp(word,"end")) {
           char useless[_MAX_STRING_SIZE_];
           ferr = fscanf(in,"%s",useless); if (ferr != 1) return(ferr);
@@ -67,6 +79,9 @@ int NavierStokes2DInitialize(void *s,void *m)
 	  }
   }
   fclose(in);
+
+  /* Scaling Reynolds number by the freestream Mach number */
+  physics->Re /= physics->Minf;
 
   /* initializing physical model-specific functions */
   solver->ComputeCFL  = NavierStokes2DComputeCFL;
@@ -88,6 +103,11 @@ int NavierStokes2DInitialize(void *s,void *m)
   int n;
   DomainBoundary  *boundary = (DomainBoundary*) solver->boundary;
   for (n = 0; n < solver->nBoundaryZones; n++)  boundary[n].gamma = physics->gamma;
+
+  /* finally, hijack the main solver's dissipation function pointer
+   * to this model's own function, since it's difficult to express 
+   * the dissipation terms in the general form                      */
+  solver->ParabolicFunction = NavierStokes2DParabolicFunction;
 
   return(0);
 }
