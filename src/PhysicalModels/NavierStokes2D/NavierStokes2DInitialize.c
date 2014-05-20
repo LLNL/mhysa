41c644c40f272fe1b8716936d5fd46fc623a6c32
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-05-20 16:13:48 -0500

    NavierStokes2D: Rank 0 reads physics-model inputs and broadcasts them..

--- src/PhysicalModels/NavierStokes2D/NavierStokes2DInitialize.c
+++ src/PhysicalModels/NavierStokes2D/NavierStokes2DInitialize.c
@@ -45,40 +45,49 @@ int NavierStokes2DInitialize(void *s,void *m)
   strcpy(physics->upw_choice,"roe");
 
   /* reading physical model specific inputs - all processes */
-  FILE *in;
-  if (!mpi->rank) printf("Reading physical model inputs from file \"physics.inp\".\n");
-  in = fopen("physics.inp","r");
-  if (!in) {
-    printf("Warning: File \"physics.inp\" not found. Using default values.\n");
-  } else {
-    char word[_MAX_STRING_SIZE_];
-    ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
-    if (!strcmp(word, "begin")){
-	    while (strcmp(word, "end")){
-		    ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
-        if (!strcmp(word, "gamma")) { 
-          ferr = fscanf(in,"%lf",&physics->gamma); if (ferr != 1) return(1);
-        } else if (!strcmp(word,"upwinding")) {
-          ferr = fscanf(in,"%s",physics->upw_choice); if (ferr != 1) return(1);
-        } else if (!strcmp(word,"Pr")) {
-          ferr = fscanf(in,"%lf",&physics->Pr); if (ferr != 1) return(1);
-        } else if (!strcmp(word,"Re")) {
-          ferr = fscanf(in,"%lf",&physics->Re); if (ferr != 1) return(1);
-        } else if (!strcmp(word,"Minf")) {
-          ferr = fscanf(in,"%lf",&physics->Minf); if (ferr != 1) return(1);
-        } else if (strcmp(word,"end")) {
-          char useless[_MAX_STRING_SIZE_];
-          ferr = fscanf(in,"%s",useless); if (ferr != 1) return(ferr);
-          printf("Warning: keyword %s in file \"physics.inp\" with value %s not ",word,useless);
-          printf("recognized or extraneous. Ignoring.\n");
+  if (!mpi->rank) {
+    FILE *in;
+    printf("Reading physical model inputs from file \"physics.inp\".\n");
+    in = fopen("physics.inp","r");
+    if (!in) printf("Warning: File \"physics.inp\" not found. Using default values.\n");
+    else {
+      char word[_MAX_STRING_SIZE_];
+      ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
+      if (!strcmp(word, "begin")){
+	      while (strcmp(word, "end")){
+		      ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
+          if (!strcmp(word, "gamma")) { 
+            ferr = fscanf(in,"%lf",&physics->gamma); if (ferr != 1) return(1);
+          } else if (!strcmp(word,"upwinding")) {
+            ferr = fscanf(in,"%s",physics->upw_choice); if (ferr != 1) return(1);
+          } else if (!strcmp(word,"Pr")) {
+            ferr = fscanf(in,"%lf",&physics->Pr); if (ferr != 1) return(1);
+          } else if (!strcmp(word,"Re")) {
+            ferr = fscanf(in,"%lf",&physics->Re); if (ferr != 1) return(1);
+          } else if (!strcmp(word,"Minf")) {
+            ferr = fscanf(in,"%lf",&physics->Minf); if (ferr != 1) return(1);
+          } else if (strcmp(word,"end")) {
+            char useless[_MAX_STRING_SIZE_];
+            ferr = fscanf(in,"%s",useless); if (ferr != 1) return(ferr);
+            printf("Warning: keyword %s in file \"physics.inp\" with value %s not ",word,useless);
+            printf("recognized or extraneous. Ignoring.\n");
+          }
         }
-      }
-	  } else {
-    	fprintf(stderr,"Error: Illegal format in file \"physics.inp\".\n");
-      return(1);
-	  }
+	    } else {
+    	  fprintf(stderr,"Error: Illegal format in file \"physics.inp\".\n");
+        return(1);
+	    }
+    }
+    fclose(in);
   }
-  fclose(in);
+
+#ifndef serial
+  IERR MPIBroadcast_character(physics->upw_choice,_MAX_STRING_SIZE_,0,&mpi->world); CHECKERR(ierr);
+  IERR MPIBroadcast_double(&physics->gamma,1,0,&mpi->world);                        CHECKERR(ierr);
+  IERR MPIBroadcast_double(&physics->Pr   ,1,0,&mpi->world);                        CHECKERR(ierr);
+  IERR MPIBroadcast_double(&physics->Re   ,1,0,&mpi->world);                        CHECKERR(ierr);
+  IERR MPIBroadcast_double(&physics->Minf ,1,0,&mpi->world);                        CHECKERR(ierr);
+#endif
 
   /* Scaling the Reynolds number with the M_inf */
   physics->Re /= physics->Minf;
