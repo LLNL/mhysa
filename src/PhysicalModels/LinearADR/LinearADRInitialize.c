652c01abc4a2365a9daeb5816f59c4bb4527c38a
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-05-20 16:08:08 -0500

    Linear Advection-Diffusion-Reaction: Rank 0 reads physics-model inputs and broadcasts them..

--- src/PhysicalModels/LinearADR/LinearADRInitialize.c
+++ src/PhysicalModels/LinearADR/LinearADRInitialize.c
@@ -30,39 +30,46 @@ int LinearADRInitialize(void *s,void *m)
   _ArraySetValue_(physics->d,solver->ndims*solver->nvars,0.0);
 
   /* reading physical model specific inputs - all processes */
-  FILE *in;
-  if (!mpi->rank) printf("Reading physical model inputs from file \"physics.inp\".\n");
-  in = fopen("physics.inp","r");
-  if (!in) {
-    fprintf(stderr,"Error: File \"physics.inp\" not found.\n");
-    return(1);
-  } else {
-    char word[_MAX_STRING_SIZE_];
-    ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
-    if (!strcmp(word, "begin")){
-	    while (strcmp(word, "end")){
-		    ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
-        if (!strcmp(word, "advection")) {
-          /* read advection coefficients */
-          for (i=0; i<solver->ndims*solver->nvars; i++) ferr = fscanf(in,"%lf",&physics->a[i]);
-          if (ferr != 1) return(1);
-        } else if (!strcmp(word, "diffusion")) {
-          /* read diffusion coefficients */
-          for (i=0; i<solver->ndims*solver->nvars; i++) ferr = fscanf(in,"%lf",&physics->d[i]);
-          if (ferr != 1) return(1);
-        } else if (strcmp(word,"end")) {
-          char useless[_MAX_STRING_SIZE_];
-          ferr = fscanf(in,"%s",useless); if (ferr != 1) return(ferr);
-          printf("Warning: keyword %s in file \"physics.inp\" with value %s not ",word,useless);
-          printf("recognized or extraneous. Ignoring.\n");
-        }
-      }
-	  } else {
-    	fprintf(stderr,"Error: Illegal format in file \"physics.inp\".\n");
+  if (!mpi->rank) {
+    FILE *in;
+    printf("Reading physical model inputs from file \"physics.inp\".\n");
+    in = fopen("physics.inp","r");
+    if (!in) {
+      fprintf(stderr,"Error: File \"physics.inp\" not found.\n");
       return(1);
-	  }
+    } else {
+      char word[_MAX_STRING_SIZE_];
+      ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
+      if (!strcmp(word, "begin")){
+	      while (strcmp(word, "end")){
+		      ferr = fscanf(in,"%s",word); if (ferr != 1) return(1);
+          if (!strcmp(word, "advection")) {
+            /* read advection coefficients */
+            for (i=0; i<solver->ndims*solver->nvars; i++) ferr = fscanf(in,"%lf",&physics->a[i]);
+            if (ferr != 1) return(1);
+          } else if (!strcmp(word, "diffusion")) {
+            /* read diffusion coefficients */
+            for (i=0; i<solver->ndims*solver->nvars; i++) ferr = fscanf(in,"%lf",&physics->d[i]);
+            if (ferr != 1) return(1);
+          } else if (strcmp(word,"end")) {
+            char useless[_MAX_STRING_SIZE_];
+            ferr = fscanf(in,"%s",useless); if (ferr != 1) return(ferr);
+            printf("Warning: keyword %s in file \"physics.inp\" with value %s not ",word,useless);
+            printf("recognized or extraneous. Ignoring.\n");
+          }
+        }
+	    } else {
+    	  fprintf(stderr,"Error: Illegal format in file \"physics.inp\".\n");
+        return(1);
+	    }
+    }
+    fclose(in);
   }
-  fclose(in);
+
+#ifndef serial
+  IERR MPIBroadcast_double(physics->a,solver->ndims*solver->nvars,0,&mpi->world); CHECKERR(ierr);
+  IERR MPIBroadcast_double(physics->d,solver->ndims*solver->nvars,0,&mpi->world); CHECKERR(ierr);
+#endif
 
   /* initializing physical model-specific functions */
   solver->ComputeCFL         = LinearADRComputeCFL;
