8c00a0da06f89441aee4daa5969cd938683e3183
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-09-03 17:53:10 -0500

    FirstDerivative: Added first order forward and backward approximations

---
+++ src/FirstDerivative/FirstDerivativeFirstOrder.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <basic.h>
+#include <mathfunctions.h>
+#include <arrayfunctions.h>
+#include <firstderivative.h>
+
+#include <mpivars.h>
+#include <hypar.h>
+typedef MPIVariables  MPIContext;
+typedef HyPar         SolverContext;
+
+#ifdef with_omp
+#include <omp.h>
+#endif
+
+/* 
+  First order differencing
+*/
+
+int FirstDerivativeFirstOrder(double *Df,double *f,int dir,int bias,void *s,void *m)
+{
+  SolverContext *solver = (SolverContext*) s;
+  int           i, j, v;
+
+  int ghosts = solver->ghosts;
+  int ndims  = solver->ndims;
+  int nvars  = solver->nvars;
+  int *dim   = solver->dim_local;
+
+
+  if ((!Df) || (!f)) {
+    fprintf(stderr, "Error in FirstDerivativeSecondOrder(): input arrays not allocated.\n");
+    return(1);
+  }
+
+  /* create index and bounds for the outer loop, i.e., to loop over all 1D lines along
+     dimension "dir"                                                                    */
+  int indexC[ndims], index_outer[ndims], bounds_outer[ndims];
+  _ArrayCopy1D_(dim,bounds_outer,ndims); bounds_outer[dir] =  1;
+  int N_outer; _ArrayProduct1D_(bounds_outer,ndims,N_outer);
+
+#pragma omp parallel for schedule(auto) default(shared) private(i,j,v,index_outer,indexC)
+  for (j=0; j<N_outer; j++) {
+    _ArrayIndexnD_(ndims,j,bounds_outer,index_outer,0);
+    _ArrayCopy1D_(index_outer,indexC,ndims);
+    /* left boundary */
+    for (i = -ghosts; i < -ghosts+1; i++) {
+      int qC, qR, qRR;
+      indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC );
+      indexC[dir] = i+1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qR );
+      for (v=0; v<nvars; v++)  Df[qC*nvars+v] = f[qR*nvars+v]-f[qC*nvars+v];
+    }
+    /* interior */
+    for (i = -ghosts+1; i < dim[dir]+ghosts-1; i++) {
+      int qC, qL, qR;
+      indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC );
+      indexC[dir] = i-1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qL);
+      indexC[dir] = i+1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qR);
+      for (v=0; v<nvars; v++)  Df[qC*nvars+v] = max(bias,0)*f[qR*nvars+v]-bias*f[qC*nvars+v]+min(bias,0)*f[qL*nvars+v];
+    }
+    /* right boundary */
+    for (i = dim[dir]+ghosts-1; i < dim[dir]+ghosts; i++) {
+      int qLL, qL, qC;
+      indexC[dir] = i-1; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qL );
+      indexC[dir] = i  ; _ArrayIndex1D_(ndims,dim,indexC,ghosts,qC );
+      for (v=0; v<nvars; v++)  Df[qC*nvars+v] = f[qC*nvars+v]-f[qL*nvars+v];
+    }
+  }
+  
+  return(0);
+}
