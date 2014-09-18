89666370f89c8b98a56c9829f386332764314eb1
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-09-18 10:50:10 -0500

    FPPowerSystem1Bus: Using the steady (t -> infinity) formulations of the dissipation terms

--- src/PhysicalModels/FPPowerSystem1Bus/FPPowerSystem1BusFunctions.c
+++ src/PhysicalModels/FPPowerSystem1Bus/FPPowerSystem1BusFunctions.c
@@ -20,21 +20,28 @@ double FPPowerSystem1BusDissipationFunction(int dir1,int dir2,void *p,double t)
 {
   FPPowerSystem1Bus *params = (FPPowerSystem1Bus*) p;
 
+  double sigma  = params->sigma;
+  double omegaS = params->omegaS;
+  double omegaB = params->omegaB;
+  double lambda = params->lambda;
+  double H      = params->H;
+  double D      = params->D;
+
   double dissp = 0;
   if (dir1 == _YDIR_) {
-    double term = (params->sigma*params->sigma*params->omegaS*params->omegaS) / (4.0*params->H*params->H);
-    double expterm = exp(-t/params->lambda);
+
+    double term = (sigma*sigma*omegaS*omegaS) / (4.0*H*H);
+    double expterm = exp(-t/lambda);
+
     if (dir2 == _XDIR_) {
-      dissp = term * (params->lambda*params->omegaB) * (params->lambda*(1-expterm) - t*expterm);
-      /* dissp = term * params->lambda*params->omegaB*params->lambda; */
+      /* dissp = term * (lambda*omegaB) * (lambda*(1-expterm) - t*expterm); */
+      dissp = term * lambda*omegaB*lambda;
     } else if (dir2 == _YDIR_) {
-      double gamma = params->D * params->omegaS / (2.0*params->H);
-      /* dissp = term * (params->lambda/(params->lambda*gamma+1.0))
-               * (1.0 - exp(-(gamma+1.0/params->lambda)*t)); */
-      dissp = term * (params->lambda*(1-expterm) 
-              + ( gamma*params->lambda * (t*expterm-params->lambda*(1-expterm)) ) );
-      /* dissp = term * params->lambda * (1 - params->lambda*gamma); */
+      double gamma = D*omegaS / (2.0*H);
+      /* dissp = term * (lambda*(1-expterm) + (gamma*lambda*(t*expterm-lambda*(1-expterm))) ); */
+      dissp = term * params->lambda * (1 - params->lambda*gamma);
     }
+
   }
 
   return(dissp);
