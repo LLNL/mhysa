a1db3440184bffbd875f9d75bf25c5b8bb92e20d
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2014-10-28 14:59:09 -0500

    Bugfix: apply boundary conditions before calculating nonlinear finite-difference operator coefficients

--- src/HyParFunctions/NonLinearInterpolation.c
+++ src/HyParFunctions/NonLinearInterpolation.c
@@ -22,12 +22,16 @@ int NonLinearInterpolation(double *u,void *s,void *m,double t,int(*FluxFunction)
     int size = 1, d;
     for (d=0; d<ndims; d++) size *= (dim[d] + 2*ghosts);
 
+    /* apply boundary conditions and exchange data over MPI interfaces */
+    IERR solver->ApplyBoundaryConditions(solver,mpi,u,NULL,0,t);      CHECKERR(ierr);
+    IERR MPIExchangeBoundariesnD(ndims,nvars,dim,ghosts,mpi,u);       CHECKERR(ierr);
+
     int offset = 0;
     for (d = 0; d < ndims; d++) {
       /* evaluate cell-centered flux */
-      IERR FluxFunction(FluxC,u,d,solver,t); CHECKERR(ierr);
+      IERR FluxFunction(FluxC,u,d,solver,t);                          CHECKERR(ierr);
       /* calculate non-linear interpolation coefficients */
-      IERR solver->SetInterpLimiterVar(FluxC,u,x+offset,d,solver,mpi);
+      IERR solver->SetInterpLimiterVar(FluxC,u,x+offset,d,solver,mpi);CHECKERR(ierr);
       offset += dim[d] + 2*ghosts;
     }
   }
