2652015549729889b47011b3bdd0cf744642f665
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2015-02-06 21:54:57 -0600

    Time integration (GLM-GEE): bugfix in cleaning up

--- src/TimeIntegration/TimeGLMGEECleanup.c
+++ src/TimeIntegration/TimeGLMGEECleanup.c
@@ -9,11 +9,11 @@ int TimeGLMGEECleanup(void *s)
   if (params->B_yyt ) free(params->B_yyt);
   if (params->C_yyt ) free(params->C_yyt);
   if (params->D_yyt ) free(params->D_yyt);
-  if (params->c_yyt ) free(params->D_yyt);
+  if (params->c_yyt ) free(params->c_yyt);
   if (params->A_yeps) free(params->A_yeps);
   if (params->B_yeps) free(params->B_yeps);
   if (params->C_yeps) free(params->C_yeps);
   if (params->D_yeps) free(params->D_yeps);
-  if (params->c_yeps) free(params->D_yeps);
+  if (params->c_yeps) free(params->c_yeps);
   return(0);
 }
