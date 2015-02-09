8ee8c0d709eb30f6e7dc8835545b970551a04c9c
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2015-02-09 16:17:22 -0600

    Matlab scripts: added a function to write the input file for GLM-GEE methods

---
+++ Examples/Matlab/WriteGLMGEEInp.m
@@ -0,0 +1,12 @@
+function WriteGLMGEEInp(ee_mode)
+
+%WRITEGLMGEEINP Writes the glm_gee.inp file for HyPar
+
+fid = fopen('glm_gee.inp','w');
+fprintf(fid,'begin\n');
+fprintf(fid,'\tee_mode              %d\n',ee_mode);
+fprintf(fid,'end\n');
+fclose(fid);
+
+end
+
