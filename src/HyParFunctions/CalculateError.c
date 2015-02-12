69b19134baae5436e09b712cfda34bf06254215c
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2015-02-12 14:41:42 -0600

    Calculating relative error when solution norms are higher than machine zero

--- src/HyParFunctions/CalculateError.c
+++ src/HyParFunctions/CalculateError.c
@@ -23,7 +23,7 @@ int CalculateError(void *s,void *m)
     size *= (solver->dim_local[i]+2*solver->ghosts);
   uex = (double*) calloc (size, sizeof(double));
 
-
+  static const double tolerance = 1e-15;
   IERR ExactSolution(solver,mpi,uex,&exact_flag); CHECKERR(ierr);
 
   if (!exact_flag) {
@@ -32,6 +32,24 @@ int CalculateError(void *s,void *m)
     solver->error[0] = solver->error[1] = solver->error[2] = 0.0;
 
   } else {
+
+    /* calculate solution norms (for relative error) */
+    double solution_norm[3] = {0.0,0.0,0.0};
+    /* L1 */
+    sum = ArraySumAbsnD   (solver->nvars,solver->ndims,solver->dim_local,
+                           solver->ghosts,solver->index,solver->u);
+    global_sum = 0; MPISum_double(&global_sum,&sum,1,&mpi->world);
+    solution_norm[0] = global_sum/((double)solver->npoints_global);
+    /* L2 */
+    sum = ArraySumSquarenD(solver->nvars,solver->ndims,solver->dim_local,
+                           solver->ghosts,solver->index,solver->u);
+    global_sum = 0; MPISum_double(&global_sum,&sum,1,&mpi->world);
+    solution_norm[1] = sqrt(global_sum/((double)solver->npoints_global));
+    /* Linf */
+    sum = ArrayMaxnD      (solver->nvars,solver->ndims,solver->dim_local,
+                           solver->ghosts,solver->index,solver->u);
+    global_sum = 0; MPIMax_double(&global_sum,&sum,1,&mpi->world);
+    solution_norm[2] = global_sum;
 
     /* compute error = difference between exact and numerical solution */
     _ArrayAXPY_(solver->u,-1.0,uex,size);
@@ -54,6 +72,17 @@ int CalculateError(void *s,void *m)
     global_sum = 0; MPIMax_double(&global_sum,&sum,1,&mpi->world);
     solver->error[2] = global_sum;
 
+    /* 
+      decide whether to normalize and report relative errors, 
+      or report absolute errors.
+    */
+    if (    (solution_norm[0] > tolerance) 
+        &&  (solution_norm[1] > tolerance) 
+        &&  (solution_norm[2] > tolerance) ) {
+      solver->error[0] /= solution_norm[0];
+      solver->error[1] /= solution_norm[1];
+      solver->error[2] /= solution_norm[2];
+    }
   }
 
   free(uex);
--- src/TimeIntegration/TimeError.c
+++ src/TimeIntegration/TimeError.c
@@ -11,6 +11,9 @@ int TimeError(void *ts)
   HyPar           *solver = (HyPar*)           TS->solver;
   MPIVariables    *mpi    = (MPIVariables*)    TS->mpi;
   int             size    = solver->npoints_local_wghosts * solver->nvars;
+  double          sum     = 0.0, global_sum = 0.0;
+
+  static const double tolerance = 1e-15;
 
   if (!strcmp(solver->time_scheme,_GLM_GEE_)) {
     /* For GLM-GEE methods, calculate the norm of the estimated global error */
@@ -23,22 +26,47 @@ int TimeError(void *ts)
       _ArrayScale1D_(Uerr,(1.0/(1.0-params->gamma)),size);
     }
 
-    double sum, global_sum;
+    /* calculate solution norm for relative errors */
+    double sol_norm[3] = {0.0,0.0,0.0};
+    /* L1 */
+    sum = ArraySumAbsnD   (solver->nvars,solver->ndims,solver->dim_local,
+                           solver->ghosts,solver->index,solver->u);
+    global_sum = 0; MPISum_double(&global_sum,&sum,1,&mpi->world);
+    sol_norm[0] = global_sum/((double)solver->npoints_global);
+    /* L2 */
+    sum = ArraySumSquarenD(solver->nvars,solver->ndims,solver->dim_local,
+                           solver->ghosts,solver->index,solver->u);
+    global_sum = 0; MPISum_double(&global_sum,&sum,1,&mpi->world);
+    sol_norm[1] = sqrt(global_sum/((double)solver->npoints_global));
+    /* Linf */
+    sum = ArrayMaxnD      (solver->nvars,solver->ndims,solver->dim_local,
+                           solver->ghosts,solver->index,solver->u);
+    global_sum = 0; MPIMax_double(&global_sum,&sum,1,&mpi->world);
+    sol_norm[2] = global_sum;
+
     /* calculate L1 norm of error */
     sum = ArraySumAbsnD   (solver->nvars,solver->ndims,solver->dim_local,
-                         solver->ghosts,solver->index,Uerr);
+                           solver->ghosts,solver->index,Uerr);
     global_sum = 0; MPISum_double(&global_sum,&sum,1,&mpi->world);
     error[0] = global_sum/((double)solver->npoints_global);
     /* calculate L2 norm of error */
     sum = ArraySumSquarenD(solver->nvars,solver->ndims,solver->dim_local,
-                         solver->ghosts,solver->index,Uerr);
+                           solver->ghosts,solver->index,Uerr);
     global_sum = 0; MPISum_double(&global_sum,&sum,1,&mpi->world);
     error[1] = sqrt(global_sum/((double)solver->npoints_global));
     /* calculate Linf norm of error */
     sum = ArrayMaxnD      (solver->nvars,solver->ndims,solver->dim_local,
-                         solver->ghosts,solver->index,Uerr);
+                           solver->ghosts,solver->index,Uerr);
     global_sum = 0; MPIMax_double(&global_sum,&sum,1,&mpi->world);
     error[2] = global_sum;
+
+    if (   (sol_norm[0] > tolerance)
+        && (sol_norm[1] > tolerance)
+        && (sol_norm[2] > tolerance) ) {
+      error[0] /= sol_norm[0];
+      error[1] /= sol_norm[1];
+      error[2] /= sol_norm[2];
+    }
 
     /* write to file */
     if (!mpi->rank) {
