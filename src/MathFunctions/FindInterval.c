0c5555e90815a49c2c81109051d707117d355efd
Debojyoti Ghosh <ghosh@mcs.anl.gov>
2015-02-11 22:10:13 -0600

    Boundary treatment: when calculating boundary extent, we need to add a tolerance to the physical extent specified in the input, to allow for *rare* cases when dx is irrational, and the grid boundaries are O(roundoff-error) removed from the physical boundaries.

--- src/MathFunctions/FindInterval.c
+++ src/MathFunctions/FindInterval.c
@@ -6,11 +6,18 @@ void FindInterval(double a, double b, double *x, int N, int *imin, int *imax)
   *imax = -1;
   *imin =  N;
 
+  double min_dx = x[1] - x[0];
+  for (i = 2; i < N; i++) {
+    double dx = x[i] - x[i-1];
+    if (dx < min_dx) min_dx = dx;
+  }
+  double tol = 1e-10 * min_dx;
+
   for (i = 0; i < N; i++) {
-    if (x[i] <= b) *imax = i+1;
+    if (x[i] <= (b+tol)) *imax = i+1;
   }
   for (i = N-1; i > -1; i--) {
-    if (x[i] >= a) *imin = i;
+    if (x[i] >= (a-tol)) *imin = i;
   }
 
   return;
