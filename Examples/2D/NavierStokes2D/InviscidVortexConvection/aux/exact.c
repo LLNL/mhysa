301b2f0279bb3cf69f8075bda99ac7861d2dc3ac
Debojyoti Ghosh <debojyoti.ghosh@gmail.com>
2016-02-22 16:46:41 -0800

    NavierStokes2D Examples: changes to vortex convection

--- Examples/2D/NavierStokes2D/InviscidVortexConvection/aux/exact.c
+++ Examples/2D/NavierStokes2D/InviscidVortexConvection/aux/exact.c
@@ -16,7 +16,8 @@ int main(){
   double  tf, dt;
   char    ip_file_type[50]; strcpy(ip_file_type,"ascii");
 
-  FILE *in;
+  FILE *in, *out;
+
   printf("Reading file \"solver.inp\"...\n");
   in = fopen("solver.inp","r");
   if (!in) {
@@ -53,13 +54,6 @@ int main(){
   tf = (double)n_iter * dt; double tff = tf;
   while (tf > 20) tf -= 20; // Time period
 
-  double u_inf = 0.5;
-  double v_inf = 0.0;
-  double b = u_inf;
-  double x0 = 5.0+tf*u_inf, y0 = 5.0;
-  if (x0 > 10) x0 -= 10; //periodic domain
-  printf("Final time: %lf, Vortex center: %lf, %lf\n",tff,x0,y0);
-
 	double *x, *y, *u0, *u1, *u2, *u3;
 	x   = (double*) calloc (NI   , sizeof(double));
 	y   = (double*) calloc (NJ   , sizeof(double));
@@ -68,6 +62,13 @@ int main(){
 	u2  = (double*) calloc (NI*NJ, sizeof(double));
 	u3  = (double*) calloc (NI*NJ, sizeof(double));
 
+  double u_inf = 0.5;
+  double v_inf = 0.0;
+  double b = u_inf;
+  double x0, y0;
+
+  /* Initial solution */
+  x0 = 5.0, y0 = 5.0;
 	for (i = 0; i < NI; i++){
   	for (j = 0; j < NJ; j++){
 	  	x[i] = i*dx;
@@ -95,8 +96,94 @@ int main(){
       u3[p] = P/(GAMMA-1.0) + 0.5*rho*(u*u+v*v);
 	  }
 	}
+  if (!strcmp(ip_file_type,"ascii")) {
+    printf("Writing ASCII initial solution file initial.inp\n");
+  	out = fopen("initial.inp","w");
+    for (i = 0; i < NI; i++)  fprintf(out,"%lf ",x[i]);
+    fprintf(out,"\n");
+    for (j = 0; j < NJ; j++)  fprintf(out,"%lf ",y[j]);
+    fprintf(out,"\n");
+    for (j = 0; j < NJ; j++)	{
+	    for (i = 0; i < NI; i++)	{
+        int p = NJ*i + j;
+        fprintf(out,"%lf ",u0[p]);
+      }
+    }
+    fprintf(out,"\n");
+    for (j = 0; j < NJ; j++)	{
+	    for (i = 0; i < NI; i++)	{
+        int p = NJ*i + j;
+        fprintf(out,"%lf ",u1[p]);
+      }
+    }
+    fprintf(out,"\n");
+    for (j = 0; j < NJ; j++)	{
+	    for (i = 0; i < NI; i++)	{
+        int p = NJ*i + j;
+        fprintf(out,"%lf ",u2[p]);
+      }
+    }
+    fprintf(out,"\n");
+    for (j = 0; j < NJ; j++)	{
+	    for (i = 0; i < NI; i++)	{
+        int p = NJ*i + j;
+        fprintf(out,"%lf ",u3[p]);
+      }
+    }
+    fprintf(out,"\n");
+	  fclose(out);
+  } else if ((!strcmp(ip_file_type,"binary")) || (!strcmp(ip_file_type,"bin"))) {
+    printf("Writing binary initial solution file initial.inp\n");
+  	out = fopen("initial.inp","wb");
+    fwrite(x,sizeof(double),NI,out);
+    fwrite(y,sizeof(double),NJ,out);
+    double *U = (double*) calloc (4*NI*NJ,sizeof(double));
+    for (i=0; i < NI; i++) {
+      for (j = 0; j < NJ; j++) {
+        int p = NJ*i + j;
+        int q = NI*j + i;
+        U[4*q+0] = u0[p];
+        U[4*q+1] = u1[p];
+        U[4*q+2] = u2[p];
+        U[4*q+3] = u3[p];
+      }
+    }
+    fwrite(U,sizeof(double),4*NI*NJ,out);
+    free(U);
+    fclose(out);
+  }
 
-  FILE *out;
+  /* Exact solution */
+  x0 = 5.0+tf*u_inf, y0 = 5.0;
+  if (x0 > 10) x0 -= 10; //periodic domain
+  printf("Final time: %lf, Vortex center: %lf, %lf\n",tff,x0,y0);
+	for (i = 0; i < NI; i++){
+  	for (j = 0; j < NJ; j++){
+	  	x[i] = i*dx;
+	  	y[j] = j*dy;
+      int p = NJ*i + j;
+
+      double rx, ry;
+      rx = (x[i] - x0);
+      ry = (y[j] - y0);
+      if (rx < -5)      { rx += 10; }
+      else if (rx > 5)  { rx -= 10; }
+
+      double rsq = rx*rx + ry*ry;
+      double rho, u, v, P;
+      double du, dv;
+      rho = power(1.0 - ((GAMMA-1.0)*b*b)/(8.0*GAMMA*pi*pi) * exp(1.0-rsq), 1.0/(GAMMA-1.0));
+      P   = power(rho,GAMMA);
+      du  = - b/(2.0*pi) * exp(0.5*(1.0-rsq)) * ry;
+      dv  =   b/(2.0*pi) * exp(0.5*(1.0-rsq)) * rx;
+      u   = u_inf + du;
+      v   = v_inf + dv;
+      u0[p] = rho;
+      u1[p] = rho*u;
+      u2[p] = rho*v;
+      u3[p] = P/(GAMMA-1.0) + 0.5*rho*(u*u+v*v);
+	  }
+	}
   if (!strcmp(ip_file_type,"ascii")) {
     printf("Writing ASCII exact solution file exact.inp\n");
   	out = fopen("exact.inp","w");
