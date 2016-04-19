9a727a729b1da75ad5177eacc9a639bef874d047
Debojyoti Ghosh <ghosh5@llnl.gov>
2016-04-19 16:05:29 -0700

    Documentation: added full citation of AIAA paper

--- src/PhysicalModels/NavierStokes2D/NavierStokes2DSource.c
+++ src/PhysicalModels/NavierStokes2D/NavierStokes2DSource.c
@@ -31,7 +31,7 @@ static int NavierStokes2DSourceUpwind   (double*,double*,double*,double*,int,voi
       7th AIAA Atmospheric and Space Environments Conference, June 22-26, 2015, Dallas, TX,
       http://dx.doi.org/10.2514/6.2015-2889
     + Ghosh, D., Constantinescu, E.M., A Well-Balanced, Conservative Finite-Difference Algorithm
-      for Atmospheric Flows, AIAA Journal, http://dx.doi.org/10.2514/1.J054580.
+      for Atmospheric Flows, AIAA Journal, 54 (4), 2016, pp. 1370-1385, http://dx.doi.org/10.2514/1.J054580.
 */
 int NavierStokes2DSource(
                           double  *source,  /*!< Array to hold the computed source */
